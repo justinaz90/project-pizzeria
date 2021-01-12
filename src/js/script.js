561ba5d584bd810d33f95c0022cce1e80495c628
Justyna Szewczyk <justyna0829@gmail.com>
2021-01-12 23:31:45 +0100

    Finish price calculation function

--- src/js/script.js
+++ src/js/script.js
@@ -137,13 +137,13 @@
         event.preventDefault();
         thisProduct.processOrder();
       });
-      
+
       for(let input of thisProduct.formInputs){
         input.addEventListener('change', function(){
           thisProduct.processOrder();
         });
       }
-      
+
       thisProduct.cartButton.addEventListener('click', function(event){
         event.preventDefault();
         thisProduct.processOrder();
@@ -151,10 +151,10 @@
 
       console.log('initOrderForm: ', thisProduct);
     }
-    
+
 
     processOrder(){
-  
+
       const thisProduct = this;
 
       // convert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
@@ -169,23 +169,38 @@
 
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        console.log('param: ', paramId, param);
 
         // for every option in this category
         for(let optionId in param.options){
 
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
-          
+          console.log('option: ', optionId, option);
+
+          // check if there is param with a name of paramId in formData and if it includes optionId
+          if(formData[paramId] && formData[paramId].includes(optionId)) {
+
+            // check if the option is not default
+            if(!option.default == true) {
+              price += option.price;
+              // add option price to price variable
+            }
+          } else {
+            // check if the option is default
+            if(option.default == true) {
+              // reduce price variable
+              price -= option.price;
+            }
+          }
         }
       }
 
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
-      
+
     }
-    
+
   }
 
   const app = {
