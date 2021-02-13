83cb45035bddc08cb7f87ed533392b73a9a09ee6
Justyna Szewczyk <justyna0829@gmail.com>
2021-02-13 21:13:40 +0100

    Start with Home widget

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,3 +1,6 @@
+
+import {templates} from '.././settings.js';
+
 class Home{
   constructor(wrapper){
     const thisHome = this;
@@ -8,12 +11,26 @@ class Home{
 
   render(wrapper){
     const thisHome = this;
+
+    const generatedHTML = templates.homeWidget();
+    thisHome.dom = {};
+    thisHome.dom.wrapper = wrapper;
+    thisHome.dom.wrapper.innerHTML = generatedHTML;
   }
 
   initWidgets(){
     const thisHome = this;
+
+    thisHome.elem = document.querySelector('.main-carousel');
+    //eslint-disable-next-line no-undef
+    thisHome.flkty = new Flickity( thisHome.elem, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      autoplay: true,
+      wrapAround: true,
+    });
   }
 }
-
 
 export default Home;
